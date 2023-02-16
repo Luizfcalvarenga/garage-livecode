baad0fc73f3877050de590694f008218c78bd09d
Marcel Fonseca <marcel@lewagon.org>
2023-02-16 17:59:19 +0100

    Remove webpack and Stimulus

--- src/controllers/garage_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-
-export default class extends Controller {
-  static targets = ["carsList"]
-
-  connect() {
-    console.log("hello from garage controller!")
-  }
-}
